990268d909b8c7b3f1d45deea0e8a54d5e6ee15c
zsoltikaa <padarzsolti@gmail.com>
2025-12-18 11:00:12 +0100

    updated some css settings, added new functions to the main page.

--- Models/ViewModels/HomeViewModel.cs
+++ Models/ViewModels/HomeViewModel.cs
@@ -6,6 +6,18 @@
         public List<Category> Categories { get; set; }
         public List<Recipes> LatestRecipes { get; set; }
         public int TotalRecipes { get; set; }
+
+        // Kategóriánkénti statisztikák
+        public int TotalCookies => RecipesByCategory(1);
+        public int TotalDesserts => RecipesByCategory(2);
+        public int TotalDrinks => RecipesByCategory(3);
+        public int TotalMainDishes => RecipesByCategory(4);
+
+        private int RecipesByCategory(int categoryId)
+        {
+            if (LatestRecipes == null) return 0; 
+            return LatestRecipes.Count(r => r.CategoryId == categoryId);
+        }
     }
 
     public class RecipeDetailsViewModel
