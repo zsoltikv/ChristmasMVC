f499902cca227d796bca9b81325797d7f6dc4b77
dani <busai.dani@gmail.com>
2025-12-18 10:35:45 +0100

    Navbar responsive

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggle = document.getElementById("navToggle");
+    const menu = document.getElementById("navMenu");
+
+    if (toggle) {
+        toggle.addEventListener("click", function () {
+            menu.classList.toggle("active");
+        });
+    }
+});
